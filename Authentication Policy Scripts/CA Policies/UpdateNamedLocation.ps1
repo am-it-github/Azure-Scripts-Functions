cefdf58cafdb1c0efb4fb948834b85eebcc8a2fd
am-it <github@am-it.net>
2024-07-15 15:42:42 +0100

    updated comment to reflect the generic locid

--- Authentication Policy Scripts/CA Policies/UpdateNamedLocation.ps1
+++ Authentication Policy Scripts/CA Policies/UpdateNamedLocation.ps1
@@ -1,5 +1,5 @@
 Import-Module Microsoft.Graph.Identity.SignIns
 
 #Update the Named Location Policy, prividing a JSON formatted file of the parameters you want to update
-#The beloe LocationID is a geneirc one proviced by MS and not used in any production system
+#The below LocationID is a geneirc one proviced by MS and not used in any production system
 Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId "07a1f48d-0cbb-4c2c-8ea2-1ea00e3eb3b6" -BodyParameter $namedLocationJson
