0b34ca43851e184bb31a82af6321eb27eb626ea7
am-it <github@am-it.net>
2024-07-16 14:48:38 +0100

    Corrected Comments

---
+++ Authentication Policy Scripts/CA Policies/Named Locations/Create/CreateNewNamedLocationsFromTextFile.ps1
@@ -0,0 +1,46 @@
+
+#If you hit NTLM errors due to proxy, manually download the file and save it in same directory as this script
+#use wget to download the required file from github and save to current working directory
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt -OutFile ipv4.txt
+
+
+# Read IP addresses from the file "ipv4.txt"
+$IPScopes = Get-Content -Path "ipv4.txt"
+
+
+#Declares the ID of the tenant you want to connect to
+$tenantID = "YOUR_TENANT_ID"
+
+
+############    DO NOT CHANGE BELOW THIS LINE    #############
+# Initialize the ipRanges array
+$ipRanges = @()
+
+# Loop through each IP address in your txt file and add it to the ipRanges array
+foreach ($IPAddress in $IPScopes) {
+    $ipRanges += @{
+        "@odata.type" = "#microsoft.graph.iPv4CidrRange"
+        cidrAddress = $IPAddress
+    }
+}
+
+# Construct the final $params hashtable, using the ip ranges array from above
+$params = @{
+    "@odata.type" = "#microsoft.graph.ipNamedLocation"
+    displayName = "Blocked VPNs"
+    isTrusted = $false
+    ipRanges = $ipRanges
+}
+
+############    DO NOT CHANGE ABOVE THIS LINE    #############
+
+#Connect to MgGraph with correct Scope
+Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
+
+# Imports the Module
+Import-Module Microsoft.Graph.Identity.ConditionalAccess
+
+# Update the Named Location
+New-MgIdentityConditionalAccessNamedLocation -BodyParameter $params
+
+
--- Authentication Policy Scripts/CA Policies/Named Locations/Update/UpdateNamedLocationsFromTextFile.ps1
+++ Authentication Policy Scripts/CA Policies/Named Locations/Update/UpdateNamedLocationsFromTextFile.ps1
@@ -1,17 +1,38 @@
+
+#If you hit NTLM errors due to proxy, manually download the file and save it in same directory as this script
 #use wget to download the required file from github and save to current working directory
-#This should work but on my laptop is hitting issues due to proxy
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt -OutFile ipv4.txt
 
 
 # Read IP addresses from the file "ipv4.txt"
 $IPScopes = Get-Content -Path "ipv4.txt"
-# Declares the Named Location ID for use in the Update cmdlet later
-# YOU CAN MANUALLY DO THIS BY UNCOMMENTING THE BELOW
-# $NamedLocationID = "YOUR ID" #To get this, run "get-MgIdentityConditionalAccessNamedLocation"
-# Get the named location and store the Id in a variable
-$namedLocation = Get-MgIdentityConditionalAccessNamedLocation | Where-Object { $_.DisplayName -eq "Blocked VPNs" }
-$blockedVPNsId = $namedLocation.Id
 
+
+###### OPTION 1  #####
+# Manually Declare the Named Location ID you wish to Update
+# Declares the Named Location ID
+# $NamedLocationID = "YOUR ID" #To get this, run "get-MgIdentityConditionalAccessNamedLocation"
+###### END OF OPTION 1 ####
+
+
+###### OPTION 2 (DEFAULT) #####
+# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
+#Connect to MgGraph with correct Scope
+Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
+# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
+# Get the named location for locations matching "Blocked VPNs"
+$namedLocation = Get-MgIdentityConditionalAccessNamedLocation | Where-Object { $_.DisplayName -eq "Blocked VPNs" }
+# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
+# Extract the ID from the above variable
+$blockedVPNsId = $namedLocation.Id
+###### END OF OPTION 2 ####
+
+
+#Declares the ID of the tenant you want to connect to
+$tenantID = "YOUR_TENANT_ID"
+
+
+############    DO NOT CHANGE BELOW THIS LINE    #############
 # Initialize the ipRanges array
 $ipRanges = @()
 
@@ -31,10 +52,16 @@ $params = @{
     ipRanges = $ipRanges
 }
 
-# Update the Named Location
-# In order for this to work you must connect to MgGraph with "Connect-MgGraph -TenantID "YOUR-TENANT-ID" -Scopes Policy.ReadWrite.ConditionalAccess"
+############    DO NOT CHANGE ABOVE THIS LINE    #############
+
+#Connect to MgGraph with correct Scope
+Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
+
 # Imports the Module
 Import-Module Microsoft.Graph.Identity.ConditionalAccess
+
+# Change $blockedVPNsId to $NamedLocationID if you have gone with Option 1 Above
+# Update the Named Location
 Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId $blockedVPNsId -BodyParameter $params
 
 
