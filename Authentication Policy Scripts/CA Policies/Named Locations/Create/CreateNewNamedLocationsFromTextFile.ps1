63adf220ff2b7764ef104da62a5af8a08ac1f0b2
am-it <github@am-it.net>
2024-07-17 09:45:50 +0100

    Migrated Update and Create NL to function based calls

--- Authentication Policy Scripts/CA Policies/Named Locations/Create/CreateNewNamedLocationsFromTextFile.ps1
+++ Authentication Policy Scripts/CA Policies/Named Locations/Create/CreateNewNamedLocationsFromTextFile.ps1
@@ -1,45 +1,55 @@
+#Function to download the IP file, read IP addresses, initialize the ipRanges array, and construct the params hashtable
+function Initialize-IpRangesAndParams {
+    param (
+        [string]$url
+    )
 
-#If you hit NTLM errors due to proxy, manually download the file and save it in same directory as this script
-#use wget to download the required file from github and save to current working directory
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt -OutFile ipv4.txt
+    # File path for the downloaded IP addresses file
+    $filePath = ".\ipv4.txt"
 
+    # Download the required file from the URL
+   Invoke-WebRequest -Uri $url -OutFile $filePath
 
-# Read IP addresses from the file "ipv4.txt"
-$IPScopes = Get-Content -Path "ipv4.txt"
+    # Read IP addresses from the file
+    $IPScopes = Get-Content -Path $filePath
 
+    ## Initialize the ipRanges array
+    $ipRanges = @()
 
-#Declares the ID of the tenant you want to connect to
-$tenantID = "YOUR_TENANT_ID"
-
-
-############    DO NOT CHANGE BELOW THIS LINE    #############
-# Initialize the ipRanges array
-$ipRanges = @()
-
-# Loop through each IP address in your txt file and add it to the ipRanges array
-foreach ($IPAddress in $IPScopes) {
-    $ipRanges += @{
-        "@odata.type" = "#microsoft.graph.iPv4CidrRange"
-        cidrAddress = $IPAddress
+    ## Loop through each IP address in your txt file and add it to the ipRanges array
+    foreach ($IPAddress in $IPScopes) {
+        $ipRanges += @{
+            "@odata.type" = "#microsoft.graph.iPv4CidrRange"
+            cidrAddress = $IPAddress
+        }
     }
+
+    ## Construct the final $params hashtable, using the ip ranges array from above
+    $params = @{
+        "@odata.type" = "#microsoft.graph.ipNamedLocation"
+        displayName = "Blocked VPNs"
+        isTrusted = $false
+        ipRanges = $ipRanges
+    }
+
+    return $params
 }
 
-# Construct the final $params hashtable, using the ip ranges array from above
-$params = @{
-    "@odata.type" = "#microsoft.graph.ipNamedLocation"
-    displayName = "Blocked VPNs"
-    isTrusted = $false
-    ipRanges = $ipRanges
-}
 
-############    DO NOT CHANGE ABOVE THIS LINE    #############
+####### START OF VARIABLES TABLE #######
+# URL for the IP addresses file that is passed into the Function "Initialize-IpRangesAndParams" when called later
+$url = "https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt"
+
+# Declares the ID of the tenant you want to connect to
+$tenantID = "YOUR_TENANT_ID"
+####### END OF VARIABLES TABLE #######
+
+
+# Call the function to initialize ipRanges and construct params once
+$params = Initialize-IpRangesAndParams -url $url
 
 #Connect to MgGraph with correct Scope
-Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
-
-# Imports the Module
-# Appears to not be needed
-# Import-Module Microsoft.Graph.Identity.ConditionalAccess
+Connect-MgGraph -NoWelcome -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
 
 # Update the Named Location
 New-MgIdentityConditionalAccessNamedLocation -BodyParameter $params
--- Authentication Policy Scripts/CA Policies/Named Locations/Update/UpdateNamedLocationsFromTextFile.ps1
+++ Authentication Policy Scripts/CA Policies/Named Locations/Update/UpdateNamedLocationsFromTextFile.ps1
@@ -1,68 +1,84 @@
+# Function to download the IP file, read IP addresses, initialize the ipRanges array, and construct the params hashtable
+function Initialize-IpRangesAndParams {
+    param (
+        [string]$url
+    )
 
-#If you hit NTLM errors due to proxy, manually download the file and save it in same directory as this script
-#use wget to download the required file from github and save to current working directory
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt -OutFile ipv4.txt
+    # File path for the downloaded IP addresses file
+    $filePath = ".\ipv4.txt"
 
+    # Download the required file from the URL
+    Invoke-WebRequest -Uri $url -OutFile $filePath
 
-# Read IP addresses from the file "ipv4.txt"
-$IPScopes = Get-Content -Path "ipv4.txt"
+    # Read IP addresses from the file
+    $IPScopes = Get-Content -Path $filePath
 
+    ## Initialize the ipRanges array
+    $ipRanges = @()
 
-###### OPTION 1  #####
-# Manually Declare the Named Location ID you wish to Update
-# Declares the Named Location ID
-# $NamedLocationID = "YOUR ID" #To get this, run "get-MgIdentityConditionalAccessNamedLocation"
-###### END OF OPTION 1 ####
+    ## Loop through each IP address in your txt file and add it to the ipRanges array
+    foreach ($IPAddress in $IPScopes) {
+        $ipRanges += @{
+            "@odata.type" = "#microsoft.graph.iPv4CidrRange"
+            cidrAddress = $IPAddress
+        }
+    }
 
+    ## Construct the final $params hashtable, using the ip ranges array from above
+    $params = @{
+        "@odata.type" = "#microsoft.graph.ipNamedLocation"
+        displayName = "Blocked VPNs"
+        isTrusted = $false
+        ipRanges = $ipRanges
+    }
 
-###### OPTION 2 (DEFAULT) #####
-# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
-#Connect to MgGraph with correct Scope
-Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
-# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
-# Get the named location for locations matching "Blocked VPNs"
-$namedLocation = Get-MgIdentityConditionalAccessNamedLocation | Where-Object { $_.DisplayName -eq "Blocked VPNs" }
-# COMMENT OUT IF MANUALLY DECLAIRING A NAMED LOCATION ID ABOVE
-# Extract the ID from the above variable
-$blockedVPNsId = $namedLocation.Id
-###### END OF OPTION 2 ####
+    return $params
+}
 
+# Function to get the Named Location ID for a given display name
+function Get-NamedLocationId {
+    param (
+        [string]$displayName,
+        [string]$tenantID
+    )
 
-#Declares the ID of the tenant you want to connect to
+    # Connect to MgGraph with correct Scope
+    Connect-MgGraph -NoWelcome -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
+
+    # Get the named location for locations matching the given display name
+    $namedLocation = Get-MgIdentityConditionalAccessNamedLocation | Where-Object { $_.DisplayName -eq $displayName }
+    # Extract the ID from the above variable
+    return $namedLocation
+}
+
+####### START OF VARIABLES TABLE #######
+# URL for the IP addresses file that is passed into the Function "Initialize-IpRangesAndParams" when called later
+$url = "https://raw.githubusercontent.com/X4BNet/lists_vpn/main/output/vpn/ipv4.txt"
+
+# Declares the ID of the tenant you want to connect to
 $tenantID = "YOUR_TENANT_ID"
 
+###### OPTION 1 (DEFAULT) - Automatically get the NamedLocationID for policy matching Display Name "Blocked VPNs" using Function "Get-NamedLocationId" #####
+# Uncomment the next line to use Option 1
+$NamedLocationID = (Get-NamedLocationId -displayName "Blocked VPNs" -tenantID $tenantID).Id
 
-############    DO NOT CHANGE BELOW THIS LINE    #############
-# Initialize the ipRanges array
-$ipRanges = @()
+###### OPTION 2 -  Manually Declare the Named Location ID you wish to Update #####
+# Uncomment the next line to use Option 2
+# $NamedLocationID = "YOUR ID" # To get this, run "get-MgIdentityConditionalAccessNamedLocation"
 
-# Loop through each IP address in your txt file and add it to the ipRanges array
-foreach ($IPAddress in $IPScopes) {
-    $ipRanges += @{
-        "@odata.type" = "#microsoft.graph.iPv4CidrRange"
-        cidrAddress = $IPAddress
-    }
+####### END OF VARIABLES TABLE #######
+
+# Ensure $NamedLocationID is set
+if (-not $NamedLocationID) {
+    Write-Error "NamedLocationID is not set. Please ensure you have set it using Option 1 or Option 2."
+    exit
 }
 
-# Construct the final $params hashtable, using the ip ranges array from above
-$params = @{
-    "@odata.type" = "#microsoft.graph.ipNamedLocation"
-    displayName = "Blocked VPNs"
-    isTrusted = $false
-    ipRanges = $ipRanges
-}
+# Call the function to initialize ipRanges and construct params once
+$params = Initialize-IpRangesAndParams -url $url
 
-############    DO NOT CHANGE ABOVE THIS LINE    #############
+# Connect to MgGraph with correct Scope
+Connect-MgGraph -NoWelcome -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
 
-#Connect to MgGraph with correct Scope
-Connect-MgGraph -TenantID $tenantID -Scopes Policy.ReadWrite.ConditionalAccess
-
-# Imports the Module
-# Appears to not be needed
-# Import-Module Microsoft.Graph.Identity.ConditionalAccess
-
-# Change $blockedVPNsId to $NamedLocationID if you have gone with Option 1 Above
 # Update the Named Location
-Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId $blockedVPNsId -BodyParameter $params
-
-
+Update-MgIdentityConditionalAccessNamedLocation -NamedLocationId $NamedLocationID -BodyParameter $params
